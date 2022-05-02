a1ef08445e33e0d0537b431642596b6994f06f4f
BuildTools <unconfigured@null.spigotmc.org>
2022-05-02 19:13:58 -0400

    removed redundant access public access modifiers in SoundInterface

--- src/com/Music/SoundInterface.java
+++ src/com/Music/SoundInterface.java
@@ -2,11 +2,11 @@ package com.Music;
 
 public interface SoundInterface {
 
-    //Place the notes in order from 0 to 88 in an array
-    public void toNoteNumber();
+    //Place the note directory in order from 0 to 88 in an array
+    void toNoteNumber();
 
     //Subtract highest number from lowest number to get interval size
-    public int compareNotes();
+    int compareNotes();
 
 
 }
