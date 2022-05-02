bdd41bce93e1ed30784e75603f2cd011be0f167e
BuildTools <unconfigured@null.spigotmc.org>
2022-05-02 15:14:27 -0400

    JFrame size adapts to resolution

--- src/com/Music/Swing.java
+++ src/com/Music/Swing.java
@@ -1,12 +1,29 @@
 package com.Music;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Swing {
     JFrame frame = new JFrame();
+    Dimension tool = Toolkit.getDefaultToolkit().getScreenSize();
+    private final int scaledMonWidth = (int) tool.getWidth() / 2;
+    private final int scaledMonHeight = (int) tool.getHeight() / 2;
 
     public void swingEnable(){
+        System.out.println("Resolution is " + scaledMonWidth + " " + scaledMonHeight);
       frame.setVisible(true);
-      frame.setSize(600, 400);
+      swingSettings();
+      placeSwingElements();
     }
+
+    public void swingSettings(){
+        frame.setSize(scaledMonWidth, scaledMonHeight);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+
+    public void placeSwingElements(){
+
+    }
+
 }
