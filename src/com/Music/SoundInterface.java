0b3a5f1bfbb9646a505771a0b45fabdf47e996ef
BuildTools <unconfigured@null.spigotmc.org>
2022-05-02 18:44:33 -0400

    Sound now implement the SoundInterface

--- src/com/Music/SoundInterface.java
+++ src/com/Music/SoundInterface.java
@@ -2,12 +2,6 @@ package com.Music;
 
 public interface SoundInterface {
 
-    //Plays a random sound
-    public void playRandomSound();
-
-    //Gets the directory of the last played sound
-    public void getSoundDir();
-
     //Place the notes in order from 0 to 88 in an array
     public void toNoteNumber();
 
