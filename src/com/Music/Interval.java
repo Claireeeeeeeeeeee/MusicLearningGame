899a77c0e19e2542f9b0c1e3f160bdacf5b4b1f0
Braskx <bwf@xplornet.ca>
2022-05-03 17:36:49 -0400

    Class fields, and methods added

--- src/com/Music/Interval.java
+++ src/com/Music/Interval.java
@@ -1,8 +1,26 @@
 package com.Music;
 
 public class Interval extends Sound{
+    private int intervalDistance;
+    private String note1, note2;
 
+    //GETTERS
+    public int getIntervalDistance(){
+        return intervalDistance;
+    }
+
+    //SETTERS
+    public void setIntervalDistance(int distance){
+        this.intervalDistance = distance;
+    }
+
+    //Plays two sounds, places the note names in a field, and returns the distance between the two sounds
     public void compareInterval(){
+
+    }
+
+    //User guesses the interval and is asked if they want to try again
+    public void chooseInterval(){
 
     }
 
