7698d94b3272def6c4d40b1614c33ebdb6239333
Braskx <bwf@xplornet.ca>
2022-05-03 04:19:31 -0400

    Utilizes noteToNumber method

--- src/com/Music/Main.java
+++ src/com/Music/Main.java
@@ -3,6 +3,8 @@ package com.Music;
 public class Main {
 
     public static void main(String[] args){
+        Sound sound = new Sound();
+        sound.noteToNumber();
         Tui t = new Tui();
     }
 }
