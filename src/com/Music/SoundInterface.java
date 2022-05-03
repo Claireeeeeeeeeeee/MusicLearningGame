609d3d0898145b3d030a1b200863fec66b3ac462
Braskx <bwf@xplornet.ca>
2022-05-03 04:22:00 -0400

    changed toNoteNumber to noteToNumber

--- src/com/Music/SoundInterface.java
+++ src/com/Music/SoundInterface.java
@@ -3,7 +3,7 @@ package com.Music;
 public interface SoundInterface {
 
     //Place the note directory in order from 0 to 88 in an array
-    void toNoteNumber();
+    void noteToNumber();
 
     //Subtract highest number from lowest number to get interval size
     int compareNotes();
