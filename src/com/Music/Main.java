92842b0f9fff828fbe1418bc44ec228dbc119bfb
Braskx <bwf@xplornet.ca>
2022-05-04 16:56:01 -0400

    Removed playToSound method

--- src/com/Music/Main.java
+++ src/com/Music/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args){
         Sound sound = new Sound();
-        sound.noteToNumber();
-        //Bryson, please don't remove the noteToNumber method and then wonder why you can't manually play notes from the array.
         Tui t = new Tui();
     }
 }
