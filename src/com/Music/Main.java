7005485d0a26d85acbee9b0b4eecb0a681e8e6c7
Braskx <bwf@xplornet.ca>
2022-05-03 20:47:49 -0400

    comment so to not remove noteToNumber method

--- src/com/Music/Main.java
+++ src/com/Music/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args){
         Sound sound = new Sound();
         sound.noteToNumber();
+        /*Bryson, please don't remove the noteToNumber method and then wonder why you can't manually play notes from the array.
+        You're going to go crazy
+        From: Bryson
+         */
         Tui t = new Tui();
     }
 }
