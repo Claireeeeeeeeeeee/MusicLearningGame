c592bedad8eee9ea7a6b274f13a1988cdf445270
Braskx <bwf@xplornet.ca>
2022-05-04 16:03:20 -0400

    Fixed ArrayOutOfBounds on playSound. Added playResponse, and playGame methods.

--- src/com/Music/Interval.java
+++ src/com/Music/Interval.java
@@ -8,21 +8,35 @@ public class Interval extends Sound{
 
 
     //plays two notes, and compares the two
-    //Warning: Currently throws OutOfBounds due to the first note being too low
-    //Will fix later I'm tired
     @Override
-    public void playSound() throws ArrayIndexOutOfBoundsException {
-        int note1 = rand.nextInt(88);
-        int note2 = note1 - rand.nextInt(13);
+    public void playSound(){
+        int note1 = rand.nextInt(75) + 12;
+        int note2 = note1 - rand.nextInt(1,13);
         intervalSize = note1 - note2;
+        System.out.println(note1 + " " + note2);
         super.playSound(note1);
         super.playSound(note2);
-        System.out.println(intervalSize);
     }
 
     //User guesses the interval and is asked if they want to try again
-    public void guessInterval(){
+    public void playResponse(){
+        System.out.println("1:Minor Second\n2:Major Second\n 3:Minor Third\n4:Major Third\n5:Fourth\n6:Tritone 7:Fifth\n" +
+                "8:Augmented\n9:Sixth\n10:Dominant Seventh\n11:Major Seventh\n12:Octave");
+        int answer = input.nextInt();
+        if(answer == intervalSize){
+            System.out.println("You Win! Would you like to try again?");
+            answer = input.next().toUpperCase().charAt(0);
+            if(answer == 'Y'){
+                playGame();
+            }else{
+                return;
+            }
+        }
+    }
 
+    public void playGame(){
+        playSound();
+        playResponse();
     }
 
 }
