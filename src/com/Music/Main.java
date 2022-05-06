bdd654afbe1dc2d14b4beec30f634d9f0d4c96dd
Braskx <bwf@xplornet.ca>
2022-05-06 19:35:15 -0400

    added objects from tui

--- src/com/Music/Main.java
+++ src/com/Music/Main.java
@@ -1,9 +1,16 @@
 package com.Music;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     //FOR GOD SAKES PLEASE MAKE THE SOUNDNAME EQUAL TO THE INSTANCE NAME
     //Default Sound object. Used to access Sound methods, and static variables
     Sound cdp220r_stgrpno = new Sound("cdp220r\\stgrpno","cdp220r_stgrpno.wav",true);
+    Swing mygui = new Swing(2);
+    Scanner input = new Scanner(System.in);
+    Interval interval = new Interval();
+    Random rand = new Random();
 
     public static void main(String[] args){
         Tui t = new Tui();
