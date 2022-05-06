5cb4bc47202f1e3c5acb1397f54d9fe5d576683e
Braskx <bwf@xplornet.ca>
2022-05-06 19:34:42 -0400

    Added side and center panel

--- src/com/Music/Swing.java
+++ src/com/Music/Swing.java
@@ -1,6 +1,7 @@
 package com.Music;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class Swing extends JFrame{
@@ -8,7 +9,6 @@ public class Swing extends JFrame{
     private int monWidth = (int) tool.getWidth();
     private int monHeight = (int) tool.getHeight();
     int sizeModifier;
-    ImageIcon image = new ImageIcon("pianoicon.png");
 
     //Constructors
 
@@ -24,7 +24,6 @@ public class Swing extends JFrame{
 }
 
     public void swingEnable(){
-      this.setVisible(true);
       swingSettings();
       placeSwingElements();
     }
@@ -32,14 +31,35 @@ public class Swing extends JFrame{
     //Methods
 
     public void swingSettings(){
+        this.setVisible(true);
+        this.setTitle("Gustav");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(true);
+        this.setResizable(false);
         this.setSize(monWidth, monHeight);
     }
 
     public void placeSwingElements(){
+       int sidePanelSize = monWidth / 3;
+       int centerPanelSize = monWidth - sidePanelSize;
+
+        ImageIcon image = new ImageIcon("pianoicon.png");
+        JPanel sidePanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+        Border border = BorderFactory.createLineBorder(Color.green,3);
+
+        sidePanel.setBackground(new Color(0xfef3bd));
+        sidePanel.setBounds(0,0,sidePanelSize,monHeight);
+        sidePanel.setLayout(null);
+
+        centerPanel.setBackground(Color.darkGray);
+        centerPanel.setBounds(sidePanelSize,0,centerPanelSize,monHeight);
+        centerPanel.setLayout(null);
+
+        this.setLayout(null);
         this.setIconImage(image.getImage());
-        this.getContentPane().setBackground(new Color(30,30,30));
+        this.getContentPane().setBackground(new Color(255,255,255));
+        this.add(sidePanel);
+        this.add(centerPanel);
     }
 
 }
