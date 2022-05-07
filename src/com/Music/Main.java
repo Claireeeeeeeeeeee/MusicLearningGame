eee07f64955562f686be0e461a4365811562464c
Braskx <bwf@xplornet.ca>
2022-05-06 20:22:58 -0400

    gui now called in main

--- src/com/Music/Main.java
+++ src/com/Music/Main.java
@@ -7,12 +7,13 @@ public class Main {
     //FOR GOD SAKES PLEASE MAKE THE SOUNDNAME EQUAL TO THE INSTANCE NAME
     //Default Sound object. Used to access Sound methods, and static variables
     Sound cdp220r_stgrpno = new Sound("cdp220r\\stgrpno","cdp220r_stgrpno.wav",true);
-    Swing mygui = new Swing(2);
     Scanner input = new Scanner(System.in);
     Interval interval = new Interval();
     Random rand = new Random();
 
     public static void main(String[] args){
+        Swing mygui = new Swing(2);
+        mygui.swingEnable();
         Tui t = new Tui();
     }
 }
