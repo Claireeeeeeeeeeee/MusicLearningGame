c1ed522115fea0450da1adedcaa399cd313fdb8e
Braskx <bwf@xplornet.ca>
2022-05-09 16:13:09 -0400

    removed unused instrument selection code until I actually get more instruments recorded

--- src/com/Music/Tui.java
+++ src/com/Music/Tui.java
@@ -1,8 +1,5 @@
 package com.Music;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class Tui extends Main {
     static String selected;
 
@@ -14,13 +11,12 @@ public class Tui extends Main {
 
     public void chooseInstrument(){
         try {
-            for (int i = 0; i <= cdp220r_stgrpno.soundFont.size() - 1; i++) {
+            for (int i = 0; i <= sound.soundFontPretty.size() - 1; i++) {
 
-                System.out.println("Choose a soundfont:\n" + i + 1 + ":" + " " + cdp220r_stgrpno.soundFont.get(i));
-                selected = cdp220r_stgrpno.soundFont.get(input.nextInt() - 1);
+                System.out.println("Choose a soundfont:\n" + i + 1 + ":" + " " + sound.soundFontPretty.get(i));
+                selected = sound.soundFontPretty.get(input.nextInt() - 1);
                 System.out.println("Selected " + selected + "\n");
-                cdp220r_stgrpno.setCurrentSoundPath(cdp220r_stgrpno.soundPath.get(i));
-                cdp220r_stgrpno.setCurrentSoundName(cdp220r_stgrpno.soundName.get(i));
+                //Actually loading the proper sound needs to still be implemented
             }
         }catch(IndexOutOfBoundsException e){
             System.err.println("Index Out Of Bounds: " + e.getMessage() + "\nSoundfont not found");
@@ -29,14 +25,14 @@ public class Tui extends Main {
     }
 
     public void select() {
-        char select = 0;
+        char select;
         while (true) {
             System.out.println("M: ManualSound\nR: RandomSound\nI: Interval\nQ: Quit");
             select = input.next().toUpperCase().charAt(0);
             switch (select) {
-                case 'R' -> cdp220r_stgrpno.playSound(rand.nextInt(88));
-                case 'D' -> System.out.println(cdp220r_stgrpno.getPath());
-                case 'M' -> cdp220r_stgrpno.playSound();
+                case 'R' -> sound.playSound(rand.nextInt(88));
+                case 'D' -> System.out.println(sound.getPath());
+                case 'M' -> sound.playSound();
                 case 'Q' -> {
                     System.out.println("Quitting");
                     System.exit(0);
