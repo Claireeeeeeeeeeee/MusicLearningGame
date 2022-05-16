a1fafed09167be008f349571118602b85fbacbca
Braskx <bwf@xplornet.ca>
2022-05-16 14:40:16 -0400

    adds loadSound method to constructor

--- src/com/Music/Main.java
+++ src/com/Music/Main.java
@@ -4,12 +4,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static Sound sound = new Sound();
+    static Sound sound = new Sound("cdp220r\\stgrpno", "cdp220r_stgrpno.wav");
     Interval interval = new Interval();
     Random rand = new Random();
 
     public static void main(String[] args){
-        sound.loadSound("cdp220r\\stgrpno","cdp220r_stgrpno.wav");
         Swing mygui = new Swing(2);
         mygui.swingEnable();
         Tui t = new Tui();
