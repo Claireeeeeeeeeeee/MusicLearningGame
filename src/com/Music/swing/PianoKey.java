5d60b307f1aa4b454756a68083664fc12de0d957
bwf <bwf@xplornet.ca>
2022-05-19 14:57:06 -0400

    added visible arg to pianokeys

--- src/com/Music/swing/PianoKey.java
+++ src/com/Music/swing/PianoKey.java
@@ -9,17 +9,28 @@ import java.io.IOException;
 public class PianoKey extends Swing {
     String keyPath;
     int x,y;
+    JLabel pic;
 
     //Location of image, and coordinates
-    public PianoKey(String path, int x, int y, int width, int height) throws IOException {
-        keyPath = System.getProperty("user.dir") + "\\" + path;
-        System.out.println("key location: " + keyPath);
-        this.x = x;
-        this.y = y;
-        BufferedImage myPicture = ImageIO.read(new File(path));
-        JLabel pic = new JLabel(new ImageIcon(myPicture));
-        pic.setBounds(x,y,width,height);
-        centerPanel.add(pic);
+    public PianoKey(String path, int x, int y, int width, int height, boolean visible) {
+        try {
+            keyPath = System.getProperty("user.dir") + "\\" + path;
+            System.out.println("key location: " + keyPath);
+            this.x = x;
+            this.y = y;
+            BufferedImage myPicture = ImageIO.read(new File(path));
+            pic = new JLabel(new ImageIcon(myPicture));
+            pic.setBounds(x, y, width, height);
+            if (!visible) {
+                pic.setVisible(false);
+            } else centerPanel.add(pic);
+        }catch(IOException e){
+            e.getMessage();
+        }
+    }
+
+    public void setVisible(boolean value){
+        pic.setVisible(value);
     }
 
     public void keyDown(){
