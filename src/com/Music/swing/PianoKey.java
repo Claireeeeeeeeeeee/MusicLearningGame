6857b357b67b4a58c181253339571678e655374b
bwf <bwf@xplornet.ca>
2022-05-19 21:17:03 -0400

    added keyID

--- src/com/Music/swing/PianoKey.java
+++ src/com/Music/swing/PianoKey.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 public class PianoKey extends Swing {
     String keyPath;
+    int keyID;
     int x,y;
     JLabel pic;
 
@@ -29,14 +30,49 @@ public class PianoKey extends Swing {
         }
     }
 
+    public PianoKey(int id){
+        this.keyID = id;
+    }
+
+    public PianoKey(){
+
+    }
+
+    public void setKeyID(int id){
+        this.keyID = id;
+    }
+
+    public int getKeyID(){
+        return this.keyID;
+    }
+
+    public void setPath(String path) {
+        try {
+            keyPath = System.getProperty("user.dir") + "\\" + path;
+            System.out.println("key location: " + keyPath);
+            this.x = x;
+            this.y = y;
+            BufferedImage myPicture = ImageIO.read(new File(path));
+            pic = new JLabel(new ImageIcon(myPicture));
+        }catch(IOException e){
+            e.getMessage();
+        }
+    }
+
+    public void setPosition(int x, int y, int width, int height){
+        pic.setBounds(x,y,width,height);
+    }
+
     public void setVisible(boolean value){
         pic.setVisible(value);
     }
 
+    //Darken or highlight the key
     public void keyDown(){
-
+        System.out.println("Key Down");
     }
 
+    //Return the key to original state
     public void keyUp(){
 
     }
